9de14073892a75398436019bbdfcb08ba94bbdd5
oyatilload-debug <oyatilload@gmail.com>
2026-06-12 23:52:27 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import asyncio
 import logging
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-import os
+from aiohttp import web
 
-# Render'dagi Environment dan tokenni oladi
 TOKEN = os.environ.get("BOT_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
@@ -16,8 +16,20 @@ dp = Dispatcher()
 async def cmd_start(message: types.Message):
     await message.answer("Assalomu alaykum! Bot muvaffaqiyatli ishga tushdi.")
 
+async def handle(request):
+    return web.Response(text="Bot is running")
+
 async def main():
-    print("Bot ishga tushmoqda...")
+    # Web serverni Render portiga sozlash
+    app = web.Application()
+    app.router.add_get('/', handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    port = int(os.environ.get("PORT", 8080))
+    site = web.TCPSite(runner, '0.0.0.0', port)
+    await site.start()
+    
+    print(f"Bot va Web server {port} portida ishga tushdi...")
     await dp.start_polling(bot)
 
 if name == "__main__":
